4268e8f9e724718786794f54ac6f43a0ab0f9d1b
eutia <eutia@TIAGOPORTELAPC>
2023-07-03 10:03:23 -0300

    Adicionar arquivos de projeto.

---
+++ ArquivosIO/LidandoComFileStreamDireto.cs
@@ -0,0 +1,89 @@
+﻿using ArquivosIO;
+using System.Text;
+
+public class LidandoComFileStreamDireto
+{
+    public void LerArquivo(string arquivo)
+    {
+        var enderecoDoArquivo = arquivo;//"contas.txt";
+
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+
+        }
+        Console.ReadLine();
+    }
+
+
+    public void CriarArquivoCSV()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456, 7895, 4785.40, Gustavo Santos";
+
+            var encoding = Encoding.UTF8;
+
+            var bytes = encoding.GetBytes(contaComoString);
+
+            fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+
+        }
+
+    }
+
+
+
+    public void LendoArquivoExibindoClasse(string enderecoDoArquivo)
+    {
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"{contaCorrente.Titular.Nome} : Conta número {contaCorrente.Numero}, ag {contaCorrente.Agencia}, Saldo {contaCorrente.Saldo}";
+
+                Console.WriteLine(msg);
+            }
+        }
+    }
+
+    public ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        // 375 4644 2483.13 Jonatan
+        var campos = linha.Split(',');
+
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2].Replace('.', ',');
+        var nomeTitular = campos[3];
+
+        var agenciaComInt = int.Parse(agencia);
+        var numeroComInt = int.Parse(numero);
+        var saldoComDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        resultado.Depositar(saldoComDouble);
+        resultado.Titular = titular;
+
+        return resultado;
+    }
+
+   
+}
---
+++ ArquivosIO/Program.cs
@@ -0,0 +1,24 @@
+﻿using ArquivosIO;
+using System.Text;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        LidandoComFileStreamDireto l = new LidandoComFileStreamDireto();
+
+        l.LendoArquivoExibindoClasse("contas.txt");
+        //l.CriarArquivoCSV();
+        //l.LerArquivo("contasExportadas.csv");
+        Console.ReadLine();
+    }
+}
+
+/*
+ Nesta aula, exploramos alguns recursos da classe StreamReader:
+
+ReadLine para ler uma linha
+ReadToEnd para ler o arquivo por completo, carregando-o de uma única vex
+Read para ler o primeiro byte do arquivo
+EndOfStream para ler e exibir um arquivo até que o fluxo chegue ao fim
+ * */
